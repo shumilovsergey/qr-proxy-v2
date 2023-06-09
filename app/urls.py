296676e4f9a819a46a3d41685a4cc0f919ce7c72
shumilov <wumilovsergey@gmail.com>
2023-06-09 14:02:12 +0300

    test

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 from .views import RegistrationView
 from .views import HelloWorldView
-
+#
 urlpatterns = [
     path('', views.home, name='home'),
     path('api/hello/', HelloWorldView.as_view(), name='hello-world'),
