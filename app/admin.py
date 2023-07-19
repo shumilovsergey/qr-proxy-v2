67f21cedec4ddb5c5ede9520386958fc56ae88b3
shumilov sergey <wumilovsergey@gmail.com>
2023-07-19 22:16:17 +0300

    delete telegram

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Users
 from .models import Routers
-from .models import Chats
+
 
 admin.site.register(Users)
 admin.site.register(Routers)
-admin.site.register(Chats)
